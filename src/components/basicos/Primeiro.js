e3d7cdf90439c2c12b42754ddb68446ff712a018
Kelvin <kelvin.ronaldo@hotmail.com>
2020-12-29 15:45:21 -0300

    12 - Primeiro Componente 02

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,12 @@
-export default function Primeiro(){
-	return 'Primeiro Componente';
+import React from 'react';
+
+export default function Primeiro() {
+	const msg = 'Seja bem-vindo!';
+
+	return (
+		<div>
+			<h2>Primeiro Componente</h2>
+			<p> {msg} </p>
+		</div>
+	);
 }
