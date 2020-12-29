80205e60d12db006abd843eefbbfd7f5c62be09e
Kelvin <kelvin.ronaldo@hotmail.com>
2020-12-29 16:36:04 -0300

    13 - Componente com Propreidade

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,15 @@
+export default function ComParametro(props) {
+
+	const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação';
+
+	return (
+		<div>
+			<h2>{ props.titulo }</h2>
+			<p>
+				<strong>{ props.aluno }</strong> tem nota { ' ' }
+				<strong>{ props.nota }</strong> e está { ' ' }
+				<strong>{ status }</strong>
+			</p>
+		</div>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import React from 'react';
 import './index.css';
 
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 ReactDOM.render(
 	<div>
-		<Primeiro/>
+		<Primeiro></Primeiro>
+		<ComParametro
+			titulo='Situação do Aluno'
+			aluno='Kelvin Ronaldo'
+			nota={9.9}
+		/>
 	</div>,
-	document.getElementById("root")
+	document.getElementById('root')
 );
